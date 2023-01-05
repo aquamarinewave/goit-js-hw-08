c2c1ac05c85af7aa3c56996b85ef5ce822a6289a
Maryna Medynska <maryna.medynska@gmail.com>
2023-01-06 00:03:15 +0200

    Add third task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,32 +1,22 @@
 import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
 
-// Select with the DOM API
 const iframe = document.querySelector('iframe');
+
 const iframePlayer = new Player(iframe);
 
 const stoppedOn = localStorage.getItem("videoplayer-current-time");
 
-if (stoppedOn) {
-    console.log(stoppedOn);
-    iframePlayer.setCurrentTime(stoppedOn).then(function(seconds) {
-    console.log(seconds);  // seconds = the actual time that the player seeked to
-    }).catch(function(error) {
-        switch (error.name) {
-            case 'RangeError':
-                // the time was less than 0 or greater than the video’s duration
-                break;
+const rememberStopPoint = Throttle((seconds) => {
+  localStorage.setItem("videoplayer-current-time", seconds);
+}, 1000, { trailing: false });
 
-            default:
-                // some other error occurred
-                break;
-        }
-});
-
-}
-
-const onPlay = function({duration, percent, seconds}) {
-    // data is an object containing properties specific to that event
-    localStorage.setItem("videoplayer-current-time", seconds);
+const onPlay = ({seconds}) => {
+  rememberStopPoint(seconds);
 };
+
+if (stoppedOn) {
+  iframePlayer.setCurrentTime(stoppedOn);
+}
 
 iframePlayer.on('timeupdate', onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,67 @@
+import Throttle from 'lodash.throttle';
 
+const emailInput = document.querySelector('[name="email"]');
+const messageInput = document.querySelector('[name="message"]');
+const form = document.querySelector('form');
+
+const updateFormPerTime = Throttle((updatedData) => {
+  localStorage.setItem('feedback-form-state', updatedData);
+}, 500, { trailing: false });
+
+const getSavedForm = () => { 
+    const savedForm = localStorage.getItem('feedback-form-state');
+    return savedForm ? JSON.parse(savedForm) : {};
+};
+
+const formText = getSavedForm();
+
+const { email, message } = formText;
+
+const saveEmail = (email) => {
+    const formText = getSavedForm();
+    const updatedFormText = JSON.stringify({
+        ...formText,
+        email
+    });
+    updateFormPerTime(updatedFormText);
+};
+
+const saveMessage = (message) => {
+    const formText = getSavedForm();
+    const updatedFormText = JSON.stringify({
+        ...formText,
+        message
+    });
+    updateFormPerTime(updatedFormText);
+};  
+
+const clearForm = () => { 
+    emailInput.value = '';
+    messageInput.value = '';
+    localStorage.setItem('feedback-form-state', JSON.stringify({}));
+};
+
+if (email) {
+    emailInput.value = email;
+}
+
+if (message) {
+    messageInput.value = message;
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formText = getSavedForm();
+    console.log(formText);
+    clearForm();
+});
+
+emailInput.addEventListener('input', (event) => {
+    const emailText = event.target.value;
+    saveEmail(emailText);
+});
+
+messageInput.addEventListener('input', (event) => {
+    const messageText = event.target.value;
+    saveMessage(messageText);
+});
