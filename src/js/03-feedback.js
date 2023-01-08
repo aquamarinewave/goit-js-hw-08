6a7b781868c115bc3d8951dd1e3a013e6cef5a1b
Maryna Medynska <maryna.medynska@gmail.com>
2023-01-08 14:32:07 +0200

    Add changes to third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,78 +1,36 @@
 import throttle from 'lodash.throttle';
 
-// const emailInput = document.querySelector('[name="email"]');
-// const messageInput = document.querySelector('[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('form');
-// const input = document.getElementById('form').elements;
-// const textarea = document.getElementById('form').elements;
-const formInputText = {};
+
+getSavedForm();
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const formText = new FormData(form);
+    formText.forEach((name, value) => console.log(name, value));
+    form.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+});
+
+form.addEventListener('input', event => {
+    let savedForm = localStorage.getItem(LOCALSTORAGE_KEY); 
+    savedForm = savedForm ? JSON.parse(savedForm) : {};
+    savedForm[event.target.name] = event.target.value;
+    updateFormPerTime(savedForm);
+});
+
+function getSavedForm() { 
+    let savedForm = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (savedForm) {
+        savedForm = JSON.parse(savedForm);
+        Object.entries(savedForm).forEach(([name, value]) => {
+            form.elements[name].value = value;
+        })
+    }
+};
+
 
 const updateFormPerTime = throttle((updatedData) => {
-  localStorage.setItem('feedback-form-state', updatedData);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(updatedData));
 }, 500, { trailing: false });
-
-const getSavedForm = () => { 
-    const savedForm = localStorage.getItem('feedback-form-state');
-    return savedForm ? JSON.parse(savedForm) : {};
-};
-
-const formText = getSavedForm();
-
-// const { email, message } = formText;
-
-// const saveEmail = (email) => {
-//     const formText = getSavedForm();
-//     const updatedFormText = JSON.stringify({
-//         ...formText,
-//         email
-//     });
-//     updateFormPerTime(updatedFormText);
-// };
-
-// const saveMessage = (message) => {
-//     const formText = getSavedForm();
-//     const updatedFormText = JSON.stringify({
-//         ...formText,
-//         message
-//     });
-//     updateFormPerTime(updatedFormText);
-// };  
-
-const clearForm = () => { 
-    emailInput.value = '';
-    messageInput.value = '';
-    localStorage.setItem('feedback-form-state', JSON.stringify({}));
-};
-
-if (email) {
-    emailInput.value = email;
-}
-
-if (message) {
-    messageInput.value = message;
-}
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formText = getSavedForm();
-    console.log(formText);
-    clearForm();
-});
-
-form.addEventListener('change', event => {
-    console.log(event.target.name);
-    console.log(event.target.value);
-    formInputText[event.target.name] = event.target.value;
-    console.log(formInputText);
-
-});
-
-// emailInput.addEventListener('input', (event) => {
-//     const emailText = event.target.value;
-//     saveEmail(emailText);
-// });
-
-// messageInput.addEventListener('input', (event) => {
-//     const messageText = event.target.value;
-//     saveMessage(messageText);
-// });
